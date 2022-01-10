42d12a76e96761b8f17f8f957b3d706e92221425
PixelRaptor <sibandariley@gmail.com>
2022-01-10 14:54:18 +0200

    upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+
+
+function showMenu(){
+    document.getElementById('menu').classList.toggle('show')
+}
+function showProducts(){
+    document.getElementById('products').classList.toggle('show')
+}
+function showSignup(){
+    document.getElementById('signup').classList.toggle('show')
+}
+
+function showDownload(){
+    document.getElementById('download').classList.toggle('show')
+}
